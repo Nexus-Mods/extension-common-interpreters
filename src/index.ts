1ff27cf064911d2d9fcbabca72566a0e2cc7f99a
TanninOne <sherb@gmx.net>
2018-03-27 19:51:45 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import * as path from 'path';
+import * as process from 'process';
+import { log, types } from 'vortex-api';
+import * as which from 'which';
+
+function exeExtension(): string {
+  return process.platform === 'win32'
+    ? '.exe'
+    : '';
+}
+
+function findJava(): string {
+  if (process.env.JAVA_HOME === undefined) {
+    return undefined;
+  }
+  const fileName = 'java' + exeExtension();
+  // TODO: A bit too simplistic, check the registry on windows
+  return path.join(process.env.JAVA_HOME, 'bin', fileName);
+}
+
+function findPython(): string {
+  try {
+    return which.sync('python');
+  } catch (err) {
+    log('info', 'python not found', err.message);
+    return undefined;
+  }
+}
+
+const javaPath: string = findJava();
+const pythonPath: string = findPython();
+
+function init(context: types.IExtensionContext): boolean {
+  context.registerInterpreter('.jar', (input: types.IRunParameters) => {
+    if (javaPath === undefined) {
+      throw new Error('java not installed');
+    }
+    return {
+      executable: javaPath,
+      args: ['-jar', input.executable].concat(input.args),
+      options: input.options,
+    };
+  });
+
+  context.registerInterpreter('.vbs', (input: types.IRunParameters) => {
+    return {
+      executable: path.join(process.env.windir, 'system32', 'cscript.exe'),
+      args: [input.executable].concat(input.args),
+      options: input.options,
+    };
+  });
+
+  context.registerInterpreter('.py', (input: types.IRunParameters) => {
+    return {
+      executable: pythonPath,
+      args: [input.executable].concat(input.args),
+      options: input.options,
+    };
+  });
+  return true;
+}
+
+export default init;
---
+++ webpack.config.js
@@ -0,0 +1,3 @@
+let webpack = require('vortex-api/lib/util/webpack').default;
+
+module.exports = webpack('common-interpreters', __dirname);
