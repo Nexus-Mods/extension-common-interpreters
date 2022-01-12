e2588c0f9b4809f04300014ec3a77e28887ae9c5
TanninOne <seppleviathan@gmx.de>
2022-01-12 15:37:39 +0100

    updated webpack&dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,3 @@
 let webpack = require('vortex-api/bin/webpack').default;
 
-module.exports = webpack('common-interpreters', __dirname, 4);
+module.exports = webpack('common-interpreters', __dirname, 5);
